bada858276a7352bd8d0b8443627199f4c41de4b
MosabRM <92996115+MosabRM@users.noreply.github.com>
2021-11-10 21:20:18 +0500

    Add files via upload

---
+++ Account/Account/user.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Account
+{
+    class User
+    {
+        public string FirstName;
+        public string lastName;
+        public string UserName;
+        public string Email;
+        public DateTime BirthDay;
+        public double monthlysalary;
+        public string mobile;
+        private string number;
+        public string Mobile()
+        {
+            return mobile;
+        }
+        private string Name;
+        public string FullName()
+        {
+            Name = string.Concat(FirstName + lastName);
+            return Name;
+        }
+        private DateTime today = DateTime.Now;
+        private int Age;
+        public int CalAge()
+        {
+            Age = today.Year - BirthDay.Year;
+            return Age;
+        }
+    }
+}
